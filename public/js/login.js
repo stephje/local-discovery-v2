bedfa845ed362383ea2916e7d281884ef659dc1d
Richo Ha <richard.dqh@gmail.com>
2021-06-13 21:41:44 +0800

    Completed user sign-in - added to database and signing in

--- public/js/login.js
+++ public/js/login.js
@@ -31,19 +31,19 @@ document
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
+  const name = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
+  if (name && email && password) {
+    const response = await fetch('/api/users/signup', {
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/profile');
     } else {
       alert('Failed to sign up.');
     }
