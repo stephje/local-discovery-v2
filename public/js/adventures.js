2ffee5184debd362d3c7bce2b00cfb923446360f
stephje <s.jenkins3018@gmail.com>
2021-06-16 17:51:40 +0800

    Adjusted handlebars template to display correct buttons for each card type

---
+++ public/js/adventures.js
@@ -0,0 +1,5 @@
+const nextButton = document.querySelector('#nextButton');
+
+nextButton.addEventListener('click', () => {
+    location.href = "https://stephje.github.io/local-discovery/cottesloe.html"
+});
