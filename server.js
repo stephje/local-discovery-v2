5303a40d3d9791a8118c6ba75a1464fb4b0bfa8a
stephje <s.jenkins3018@gmail.com>
2021-06-16 16:02:55 +0800

    Added card data for all completed cards
    Updated handlebars template to handle artist webpage link

--- server.js
+++ server.js
@@ -40,6 +40,30 @@ Handlebars.registerHelper("ifNotStart", function (conditional, options) {
   }
 });
 
+Handlebars.registerHelper("ifClue", function (conditional, options) {
+  if (conditional === "clue") {
+    return options.fn(this);
+  }
+});
+
+Handlebars.registerHelper("ifNotClue", function (conditional, options) {
+  if (conditional !== "clue") {
+    return options.fn(this);
+  }
+});
+
+Handlebars.registerHelper("ifWaypoint", function (conditional, options) {
+  if (conditional === "waypoint") {
+    return options.fn(this);
+  }
+});
+
+Handlebars.registerHelper("ifNotWaypoint", function (conditional, options) {
+  if (conditional !== "waypoint") {
+    return options.fn(this);
+  }
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
