e9f5f05e2b157a7a4f6230fbf2e5713bc2fdab44
stephje <s.jenkins3018@gmail.com>
2021-06-19 18:30:04 +0800

    Added route redirect based on sequence number in user table

--- controllers/adventure/adventureRoutes.js
+++ controllers/adventure/adventureRoutes.js
@@ -2,25 +2,36 @@ const router = require('express').Router();
 const { Adventure, Cottesloe, userCottesloe } = require('../../models');
 const withAuth = require('../../utils/auth');
 const distFrom = require('distance-from');
-// require('dotenv').config();
 
 // root/adventure/cottesloe
 router.get('/cottesloe', withAuth, async (req, res) => {
 
     try {
         const cottesloeData = await Cottesloe.findAll({
-            order: [['title', 'ASC']],
+            order: [['sequence', 'ASC']],
         });
 
-        const adventures = cottesloeData.map((adventureInfo) => adventureInfo.get({ plain: true }));
-        const adventure = adventures[0];
+        const cottesloeArray = cottesloeData.map((adventureInfo) => adventureInfo.get({ plain: true }));
+        const adventure = cottesloeArray[0];
 
-        // const user = process.env.DB_USER;
+        const userCottesloeData = await userCottesloe.findAll({
+            where: {
+              id: req.session.user_id
+            }
+          });
 
-        res.render('adventures', {
-            adventure,
-            logged_in: req.session.logged_in,
-        });
+          const userCottesloeArray = userCottesloeData.map((adventureInfo) => adventureInfo.get({ plain: true }));
+          const userSequenceNumber = userCottesloeArray[0].sequence;
+
+          if(userSequenceNumber > 1) {
+            res.redirect(`/adventure/cottesloe/${userSequenceNumber}`)
+          } else {
+            res.render('adventures', {
+                adventure,
+                logged_in: req.session.logged_in,
+            });
+          }
+
     } catch (err) {
         res.status(500).json(err);
     }
