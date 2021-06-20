b5fd9b0ab911e3a69e2d07dcfae72f347b233a88
Richo Ha <richard.dqh@gmail.com>
2021-06-20 13:45:37 +0800

    completed user details showing in profile page

--- public/js/filter.js
+++ public/js/filter.js
@@ -23,3 +23,37 @@ document
     .querySelector('.filter-form')
     .addEventListener('submit', filterAdventures);
 
+
+getUserCottesloe()
+
+async function getUserCottesloe() {
+
+    const responseCott = await fetch(`/api/users/cottesloe`);
+    const userCottesloe = await responseCott.json();
+
+    console.log(`------------${JSON.stringify(userCottesloe)}-----------`)
+    console.log(`-------------${typeof userCottesloe}-----------`)
+
+    if (userCottesloe.dateCompletedRecent === null) {
+        document.querySelector('#cottesloe-date').innerHTML = `Not Completed`
+    } else {
+        document.querySelector('#cottesloe-date').innerHTML = `Last Completed on ${userCottesloe.dateCompletedRecent}`
+    }
+
+};
+
+getUserKingspark()
+
+async function getUserKingspark() {
+
+    const responseKings = await fetch(`/api/users/kingspark`);
+    const userKingspark = await responseKings.json();
+
+
+    if (userKingspark.dateCompletedRecent === null) {
+        document.querySelector('#kingspark-date').innerHTML = `Not Completed`
+    } else {
+        document.querySelector('#kingspark-date').innerHTML = `Last Completed on ${userKingspark.dateCompletedRecent}`
+    }
+
+};
